c6a82181ac6795e3dc859c16570d2220e98bc1fe
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-22 13:21:04 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,58 @@
+class TicTacToe 
+  
+  def initialize  
+    board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    @board = board
+  end
+  
+  WIN_COMBINATIONS = 
+  [
+  [0,1,2], 
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [6,4,2],
+  [0,3,6],
+  [2,5,8],
+  [1,4,7]
+  ]
+  
+  def display_board 
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end 
+  
+  def input_to_index(input)
+    input.to_i - 1
+  end
+  
+  def move(index, player = "X")
+    @board[index] = player
+  end
+  
+  def position_taken?(index)
+    @board[index] == "X" || @board[index] == "O"
+  end
+  
+  def valid_move?(index)
+    index.between?(0, 8) && !position_taken?(index) 
+  end
+  
+  
+  
+  def turn 
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(index)
+      move(index, current_player(board))
+      display_board
+    else 
+      turn
+    end
+  end 
+
+end
